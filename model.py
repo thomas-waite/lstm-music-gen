e0331d9ddaf1eaa0c6916f2321c0b00dc678cb39
thomas-waite <thomas.waite4@gmail.com>
2020-12-11 18:08:34 +0000

    feat: add Keras model definition

---
+++ model.py
@@ -0,0 +1,22 @@
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Dense, Activation
+import numpy as np
+
+
+def model_factory(network_input: np.ndarray, num_samples: int) -> Sequential:
+    model = Sequential()
+    model.add(LSTM(
+        256,
+        input_shape=(network_input.shape[1], network_input.shape[2]),
+        return_sequences=True
+    ))
+    model.add(Dropout(0.3))
+    model.add(LSTM(512, return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(256))
+    model.add(Dense(256))
+    model.add(Dropout(0.3))
+    model.add(Dense(num_samples))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    return model
